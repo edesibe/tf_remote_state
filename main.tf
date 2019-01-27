ad8cfc4421d3fa5603cc95d58ad86635759b3c3e
Ivan Milivojevic <ivan.milivojevic@endava.com>
2019-01-27 13:15:09 +0100

    update

--- main.tf
+++ main.tf
@@ -16,3 +16,20 @@ resource "aws_s3_bucket" "remote_state" {
     ENV  = "${var.environment}"
   }
 }
+
+resource "aws_dynamodb_table" "state-lock" {
+  name = "${var.prefix}-state-lock-${var.environment}"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags {
+    Name = "${var.prefix}-state-lock-${var.environment}"
+    ENV = "${var.environment}"
+  }
+}
