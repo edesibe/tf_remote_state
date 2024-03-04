0ca897f38cc02b9e29ac46d1750f47d82742bae7
edesibe <edesibe@gmail.com>
2024-03-04 14:05:38 +0100

    Update

--- s3.tf
+++ s3.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-  depends_on              = [aws_s3_bucket_object.this]
+  depends_on              = [aws_s3_object.this]
 }
 
 resource "aws_s3_bucket_versioning" "this" {
